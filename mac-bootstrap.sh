d13e633dc48641d94ec907e172a361817272f241
Rishiraj Purohit <purohitji007@gmail.com>
2024-06-22 22:49:21 +0200

    add bluetooth and sound setting

--- mac-bootstrap.sh
+++ mac-bootstrap.sh
@@ -1,12 +1,13 @@
-
 # Autohide the dock
 defaults write com.apple.dock "autohide" -bool "true" && killall Dock
 
+# Show battery percentage in menu bar
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
+
 # Show bluetooth in menu bar
-
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
 # Below does not work in macos sonoma
-# defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" && killall SystemUIServer
+# defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" && killall SystemUIServe
 
-
-killall SystemUIServer
-
+# Show sound icon
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Sound -int 18
