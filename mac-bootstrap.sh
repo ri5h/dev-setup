efd9f8fdb0ada06f110dfe487a368443386a6827
Rishiraj Purohit <purohitji007@gmail.com>
2024-06-22 22:42:50 +0200

    remove not working bluetooth but test killall

--- mac-bootstrap.sh
+++ mac-bootstrap.sh
@@ -3,4 +3,10 @@
 defaults write com.apple.dock "autohide" -bool "true" && killall Dock
 
 # Show bluetooth in menu bar
-defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" && killall SystemUIServer
+
+# Below does not work in macos sonoma
+# defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" && killall SystemUIServer
+
+
+killall SystemUIServer
+
