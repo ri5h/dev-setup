dfc9c13918cb7013dd6c946f6504fd845ac817d2
Rishiraj Purohit <purohitji007@gmail.com>
2024-06-22 22:07:57 +0200

    add bluetooth to menu bar

--- mac-bootstrap.sh
+++ mac-bootstrap.sh
@@ -1,1 +1,6 @@
+
+# Autohide the dock
 defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+
+# Show bluetooth in menu bar
+defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" && killall SystemUIServer
