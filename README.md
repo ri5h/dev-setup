002cd29ff997763127ad27729e328fea2fe85c56
Rishiraj Purohit <purohitji007@gmail.com>
2026-02-11 17:53:02 +0100

    some changes for mac os

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ curl https://raw.githubusercontent.com/ri5h/dev-setup/main/mac-bootstrap.sh
 sudo echo "hello brew!"
 
 curl https://raw.githubusercontent.com/ri5h/dev-setup/main/brew.sh
+
+
+
+# To setup on mac
+
+1. git clone this repo
+2. ´chmod +x ./macos/install´
+3. sudo echo "becoming sudo for install"
+4. ./macos/install
+
+
+
+Needed to do ssh setup in 1password and github 
--- macos/defaults.sh
+++ macos/defaults.sh
@@ -10,6 +10,9 @@ defaults write com.apple.dock "autohide" -bool "true"
 # Remove all default app icons from dock
 defaults write com.apple.dock persistent-apps -array
 
+# Stop macos from rearranging desktops 
+defaults write com.apple.dock "mru-spaces" -bool "false"
+
 killall Dock
 
 # Show battery percentage in menu bar
@@ -23,3 +26,15 @@ defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Blueto
 # Show sound icon
 defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Sound -int 18
 
+
+defaults write NSGlobalDomain AppleLanguages -array "en" "de"
+
+# Enable git commit signing
+git config --global gpg.format ssh
+git config --global user.name "Rishiraj Purohit"
+git config --global --add --bool push.autoSetupRemote true
+
+open -a "Google Chrome" --args --make-default-browser
+
+corepack enable
+yarn set version stable
